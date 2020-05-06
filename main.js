69d0d98c44aa54878c2ffc24b4cc19ff9a349896
unknown <niklas.nurminen@hotmail.com>
2020-05-06 23:15:06 +0300

    Scalability fixed and some CSS

--- main.js
+++ main.js
@@ -3,4 +3,28 @@ $(document).ready(function () {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
+
+  $('.top-nav .nav-link').on('click', function(){
+    $('.top-nav').removeClass('open');
+    $('.menu-toggler').removeClass('open');
+  });
+
+  $('nav a[href*="#"]').on('click', function(){
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 1000);
+  });
+
+  $('#up').on('click', function(){
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true
+  });
+
 });
